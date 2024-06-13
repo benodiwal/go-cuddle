e03a66cc3b9c4f20ad86602ff6cf74b1c7ec069e
codygeek01 <s474996633@gmail.com>
2024-06-13 11:52:15 +0530

    context handling

--- pkg/go-cuddle/context.go
+++ pkg/go-cuddle/context.go
@@ -1,1 +1,14 @@
 package gocuddle
+
+import (
+	"context"
+	"net/http"
+)
+
+func WithSession(r *http.Request, session *Session) *http.Request {
+	return r.WithContext(context.WithValue(r.Context(), "session", session))
+}
+
+func GetSession(r *http.Request) *Session {
+	return r.Context().Value("session").(*Session)
+}
