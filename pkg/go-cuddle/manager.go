d81403256c7f5d4fb39a3d28a54d6b8ffe04be9c
codygeek01 <s474996633@gmail.com>
2024-06-13 12:12:35 +0530

    sessions logic

--- pkg/go-cuddle/manager.go
+++ pkg/go-cuddle/manager.go
@@ -17,6 +17,7 @@ type Manager struct {
 
 func NewManager(options ...Option) *Manager {
 	m := &Manager{
+		name: "cookie-session",
 		sessions: make(map[string]*Session),
 		lifetime: 24 * time.Hour,
 	}
@@ -26,4 +27,70 @@ func NewManager(options ...Option) *Manager {
 	}
 
 	return m
+}
+
+func (m *Manager) NewSession(w http.ResponseWriter) *Session {
+	id := generateSessionID()
+	session := &Session {
+		ID: id,
+		Values: make(map[string]interface{}),
+		Expires: time.Now().Add(m.lifetime),
+		New: true,
+		Changed: true,
+	}
+
+	m.sessions[id] = session
+
+	http.SetCookie(w, &http.Cookie{
+		Name: m.name,
+		Value: id,
+		Expires: session.Expires,
+		Path: "/",
+		Secure: m.secure,
+		HttpOnly: m.httpOnly,
+		SameSite: m.sameSite,
+	})
+
+	return session
+}
+
+func (m *Manager) GetSession(r *http.Request) *Session {
+	cookie, err := r.Cookie(m.name)
+	if err != nil {
+		return nil
+	}
+	session, exists := m.sessions[cookie.Value]
+	if !exists || session.Expires.Before(time.Now()) {
+		return nil
+	}
+	return session
+}
+
+func (m *Manager) SaveSession(w http.ResponseWriter, session *Session) {
+    session.Expires = time.Now().Add(m.lifetime)
+    http.SetCookie(w, &http.Cookie{
+        Name:     m.name,
+        Value:    session.ID,
+        Expires:  session.Expires,
+        Path:     "/",
+        Secure:   m.secure,
+        HttpOnly: m.httpOnly,
+        SameSite: m.sameSite,
+    })
+    m.sessions[session.ID] = session
+}
+
+func (m *Manager) DestroySession(w http.ResponseWriter, r *http.Request) {
+    cookie, err := r.Cookie(m.name)
+    if err != nil {
+        return
+    }
+
+    delete(m.sessions, cookie.Value)
+    http.SetCookie(w, &http.Cookie{
+        Name:   m.name,
+        Value:  "",
+        Path:   "/",
+        MaxAge: -1,
+    })
 }
--- pkg/go-cuddle/middleware.go
+++ pkg/go-cuddle/middleware.go
@@ -1,2 +1,19 @@
 package gocuddle
 
+import "net/http"
+
+func (m *Manager) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		session := m.GetSession(r)
+		if session == nil {
+			session = m.NewSession(w)
+		}
+
+		r = WithSession(r, session)
+		next.ServeHTTP(w, r)
+
+		if session.Changed {
+			m.SaveSession(w, session)
+		}
+	})
+}
